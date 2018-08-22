a53a2d893af5b9e9470ed340cdaa811c80058352
Felix Ableitner <me@nutomic.com>
2018-08-22 13:03:12 -0500

    Use network name instead of magic number for file names

--- start.sh
+++ start.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 # --- bitcoin mainnet: f9beb4d9 (db = 0) ---
-python -u crawl.py conf/bitcoin/crawl.conf master > log/crawl.f9beb4d9.master.out 2>&1 &
-python -u crawl.py conf/bitcoin/crawl.conf slave > log/crawl.f9beb4d9.slave.1.out 2>&1 &
-python -u crawl.py conf/bitcoin/crawl.conf slave > log/crawl.f9beb4d9.slave.2.out 2>&1 &
-python -u crawl.py conf/bitcoin/crawl.conf slave > log/crawl.f9beb4d9.slave.3.out 2>&1 &
-python -u crawl.py conf/bitcoin/crawl.conf slave > log/crawl.f9beb4d9.slave.4.out 2>&1 &
-python -u crawl.py conf/bitcoin/crawl.conf slave > log/crawl.f9beb4d9.slave.5.out 2>&1 &
+mkdir -p log/bitcoin/
+python -u crawl.py conf/bitcoin/crawl.conf master > log/bitcoin/crawl.master.out 2>&1 &
+python -u crawl.py conf/bitcoin/crawl.conf slave > log/bitcoin/crawl.slave.1.out 2>&1 &
+python -u crawl.py conf/bitcoin/crawl.conf slave > log/bitcoin/crawl.slave.2.out 2>&1 &
+python -u crawl.py conf/bitcoin/crawl.conf slave > log/bitcoin/crawl.slave.3.out 2>&1 &
+python -u crawl.py conf/bitcoin/crawl.conf slave > log/bitcoin/crawl.slave.4.out 2>&1 &
+python -u crawl.py conf/bitcoin/crawl.conf slave > log/bitcoin/crawl.slave.5.out 2>&1 &
 
-python -u ping.py conf/bitcoin/ping.conf master > log/ping.f9beb4d9.master.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.1.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.2.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.3.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.4.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.5.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.6.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.7.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.8.out 2>&1 &
-python -u ping.py conf/bitcoin/ping.conf slave > log/ping.f9beb4d9.slave.9.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf master > log/bitcoin/ping.master.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.1.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.2.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.3.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.4.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.5.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.6.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.7.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.8.out 2>&1 &
+python -u ping.py conf/bitcoin/ping.conf slave > log/bitcoin/ping.slave.9.out 2>&1 &
 
-python -u resolve.py conf/bitcoin/resolve.conf > log/resolve.f9beb4d9.out 2>&1 &
+python -u resolve.py conf/bitcoin/resolve.conf > log/bitcoin/resolve.out 2>&1 &
 
-python -u export.py conf/bitcoin/export.conf > log/export.f9beb4d9.out 2>&1 &
+python -u export.py conf/bitcoin/export.conf > log/bitcoin/export.out 2>&1 &
 
-python -u seeder.py conf/bitcoin/seeder.conf > log/seeder.f9beb4d9.out 2>&1 &
+python -u seeder.py conf/bitcoin/seeder.conf > log/bitcoin/seeder.out 2>&1 &
 
-python -u pcap.py conf/bitcoin/pcap.conf > log/pcap.f9beb4d9.1.out 2>&1 &
-python -u pcap.py conf/bitcoin/pcap.conf > log/pcap.f9beb4d9.2.out 2>&1 &
-python -u pcap.py conf/bitcoin/pcap.conf > log/pcap.f9beb4d9.3.out 2>&1 &
+python -u pcap.py conf/bitcoin/pcap.conf > log/bitcoin/pcap.1.out 2>&1 &
+python -u pcap.py conf/bitcoin/pcap.conf > log/bitcoin/pcap.2.out 2>&1 &
+python -u pcap.py conf/bitcoin/pcap.conf > log/bitcoin/pcap.3.out 2>&1 &
